076c1ceaa67ce0cb07bf85b4a763a312803054f0
wangxiaobai-dd <leoeee@icloud.com>
2020-08-11 00:15:21 +0800

    exec

---
+++ build/lib/CMakeFiles/yolanda.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolanda.dir/read.cpp.o"
+  "CMakeFiles/yolanda.dir/tcp_server.cpp.o"
+  "CMakeFiles/yolanda.dir/tcp_client.cpp.o"
+  "CMakeFiles/yolanda.dir/event_loop.cpp.o"
+  "CMakeFiles/yolanda.dir/channel.cpp.o"
+  "CMakeFiles/yolanda.dir/acceptor.cpp.o"
+  "CMakeFiles/yolanda.dir/thread_pool.cpp.o"
+  "CMakeFiles/yolanda.dir/event_loop_thread.cpp.o"
+  "CMakeFiles/yolanda.dir/utils.cpp.o"
+  "CMakeFiles/yolanda.dir/event_dispatcher.cpp.o"
+  "CMakeFiles/yolanda.dir/buffer.cpp.o"
+  "CMakeFiles/yolanda.dir/tcp_connection.cpp.o"
+  "libyolanda.pdb"
+  "libyolanda.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolanda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
